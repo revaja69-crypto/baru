5108bc92e92c597a610d980286975a8d1c54a45f
revaja69-crypto <revaja69@gmail.com>
2026-04-16 14:56:47 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,19 @@
-const CACHE_NAME = 'cashflow-v1';
+const CACHE_NAME = 'cashflow-pro-v1';
 const ASSETS = [
   '/',
   '/index.html',
   '/style.css',
-  '/app.js',
   '/db.js',
+  '/app.js',
   '/manifest.json'
 ];
 
-// Install Service Worker dan Cache Aset
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Ambil dari Cache jika offline, jika tidak ambil dari network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
